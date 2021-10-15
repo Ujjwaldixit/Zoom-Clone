f99ce935308ae72b3974b355d10196c1ca588e2c
nirmal <nirmalkumar8273@gmail.com>
2021-10-15 15:47:35 +0530

    feat(MeetingRepository): created meetingRepository inside repository package

--- src/main/java/com/zoom/model/Meeting.java
+++ src/main/java/com/zoom/model/Meeting.java
@@ -4,15 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
-
 import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "meeting")
-public class Meeting {
+public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long meetingId;
 
     private String topic;
