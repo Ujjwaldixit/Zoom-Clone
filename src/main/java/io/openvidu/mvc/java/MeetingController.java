9e4ad021066fe6a74ee9c9ea4c4fba94114c1cae
Ujwal Dixit <ujjwal.dixit.17.1@mountblue.tech>
2021-10-14 21:43:12 -0400

    created meeting controller

---
+++ src/main/java/io/openvidu/mvc/java/MeetingController.java
@@ -0,0 +1,11 @@
+package io.openvidu.mvc.java;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MeetingController {
+    public void meeting()
+    {
+        System.out.println("Test Meeting controller");
+    }
+}
